7de7a93be699a854be6f75da75da92d401fc67c0
Jelmer <jelmer@plumerai.com>
2021-07-27 12:33:55 +0200

    Linting

--- practipy/iterators.py
+++ practipy/iterators.py
@@ -3,8 +3,8 @@ from typing import Callable, Iterable
 
 
 def batch(iterable: Iterable, chunksize: int, output_type: Callable = list):
-    """Iterates over iterable in chunks.`output_type` can be a class or a function,
-    and will be called to convert the islice object of the current batch to the desired
+    """Iterates over iterable in chunks.`output_type` can be a class or a function, and
+    will be called to convert the islice object of the current batch to the desired
     output format."""
     iterable = iter(iterable)
     while True:
