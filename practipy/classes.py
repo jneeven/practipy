82884bd763649c4da42766fc4f784289173046b2
Jelmer Neeven <19648598+jneeven@users.noreply.github.com>
2022-05-06 16:23:52 +0200

    Add other set functions to Dict class (#27)
    * Rename dictionary class and support intersection
    
    * Fix typo in dict method
    
    * Add other set functions to Dict class
    
    * Update dev requirements

--- practipy/classes.py
+++ practipy/classes.py
@@ -9,21 +9,58 @@ class Dict(dict):
 
     Attribute part copied from
     https://github.com/fastai/fastcore/blob/0df9c4a8e9a1756fe26fccffab8976195563c8a9/fastcore/basics.py#L231.
+
+    TO DO @jneeven: Add unit tests.
     """
 
-    # TODO: add other set methods like union, diff etc.
-    def intersect(self, *other: "Dict") -> "Dict":
-        """Intersect this dictionary with the other ones based on their keys."""
+    def difference(self, *others: "Dict") -> "Dict":
+        """Return a new dict with keys in this dict that are not in the others."""
+        diff_keys = self.keys()
+        for d in others:
+            diff_keys -= d.keys()
+
+        return self.__class__({k: self[k] for k in diff_keys})
+
+    def intersection(self, *others: "Dict") -> "Dict":
+        """Return a new dict with keys common to this dict and all others."""
         shared_keys = self.keys()
-        for d in other:
+        for d in others:
             shared_keys &= d.keys()
 
         return self.__class__({k: self[k] for k in shared_keys})
 
+    def symmetric_difference(self, *others: "Dict") -> "Dict":
+        """Return a new dict with keys from either this dict or the others but not
+        both."""
+
+        others_union = self.__class__.union_of(others)
+        keys_unique_to_self = self.keys() - others_union.keys()
+        keys_unique_to_others = others_union.keys() - self.keys()
+
+        return self.__class__(
+            {
+                **{k: self[k] for k in keys_unique_to_self},
+                **{k: others_union[k] for k in keys_unique_to_others},
+            }
+        )
+
+    def union(self, *others: "Dict") -> "Dict":
+        """Return a new dict with keys from this dict and all others."""
+        output = self.__class__(self)
+        for d in others:
+            output.update(d)
+
+        return output
+
     @classmethod
-    def intersect_all(cls, dicts: Sequence["Dict"]) -> "Dict":
+    def intersection_of(cls, dicts: Sequence["Dict"]) -> "Dict":
         """Intersect a sequence of dictionaries based on their keys."""
         return dicts[0].intersect(*dicts[1:])
+
+    @classmethod
+    def union_of(cls, dicts: Sequence["Dict"]) -> "Dict":
+        """Return a new dict that is the union of all others."""
+        return dicts[0].union(*dicts[1:])
 
     def __getattr__(self, k):
         if k in self:
