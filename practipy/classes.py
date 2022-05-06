ae0fd2175f3d2fb516c9e2a1991e7f5280a7c90f
Jelmer Neeven <19648598+jneeven@users.noreply.github.com>
2022-05-06 14:00:37 +0200

    Rename dictionary class and support intersection (#25)

--- practipy/classes.py
+++ practipy/classes.py
@@ -1,9 +1,29 @@
-class AttrDict(dict):
-    """`dict` subclass that also provides access to keys as attributes.
+from typing import Sequence
 
-    Copied from
+
+class Dict(dict):
+    """Improved dictionary class that:
+
+    - Provides access to keys as attributes.
+    - Has some of the same operators as sets, e.g. can be intersected with another one.
+
+    Attribute part copied from
     https://github.com/fastai/fastcore/blob/0df9c4a8e9a1756fe26fccffab8976195563c8a9/fastcore/basics.py#L231.
     """
+
+    # TODO: add other set methods like union, diff etc.
+    def intersect(self, *other: "Dict") -> "Dict":
+        """Intersect this dictionary with the other ones based on their keys."""
+        shared_keys = self.keys()
+        for d in other:
+            shared_keys &= d.keys()
+
+        return self.__class__({k: self[k] for k in shared_keys})
+
+    @classmethod
+    def intersect_all(cls, dicts: Sequence["Dict"]) -> "Dict":
+        """Intersect a sequence of dictionaries based on their keys."""
+        return dicts[0].intersection(*dicts[1:])
 
     def __getattr__(self, k):
         if k in self:
